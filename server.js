0fc443675de8a9f6b05b8853a1f13c98d0b9af47
soloprime9 <primeindia5750@gmail.com>
2025-01-07 02:22:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(cors({
-    origin:  "https://computer-coou.vercel.app"
+    origin:  "https://computer-xrfg.vercel.app"
     // origin: "http://localhost:3000"
 
 }));

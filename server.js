3b0f913aeed6ea6e16e19dc5c514c890d8eed46d
soloprime9 <primeindia5750@gmail.com>
2025-02-17 06:23:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ app.get("/", (req, res) => {
     res.send("Hello Mafia");
 })
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // app.use(express.static(path.join(__dirname, 'uploads')));
 

0e77eeecb71d444cc7f2758d34bbb11502891930
soloprime9 <primeindia5750@gmail.com>
2025-02-17 07:58:46 +0530

    Update Post.js

--- routers/Post.js
+++ routers/Post.js
@@ -81,7 +81,7 @@ router.post("/upload",verifyToken, upload.single("file"), async (req, res) => {
             // Upload Image to Cloudinary
             const UploadPromise = new Promise ((resolve, reject ) => {
                 
-            const stream = cloudinary.uploader.upload_stream({ resource_type: "auto" }, (error, result) = error ? reject(error) : resolve(result);
+            const stream = cloudinary.uploader.upload_stream({ resource_type: "auto" }, (error, result) => error ? reject(error) : resolve(result);
                 );
             stream.end(req.file.buffer);
             });

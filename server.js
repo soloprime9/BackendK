c22474b7fdca71cc8055b7d4912937138ebe85fa
soloprime9 <primeindia5750@gmail.com>
2025-02-17 06:19:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,10 +17,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use(cors({
-    origin:  "https://computer-xrfg.vercel.app",
-    // origin: "http://localhost:3000"
-    methods: "GET, POST, PUT, DELETE",
-  allowedHeaders: "Content-Type, Authorization"
+    origin: ['https://computer-xrfg.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
 
 }));
 

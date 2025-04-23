8fd67c072920ce382f1e36457e20f587e83d7958
soloprime9 <primeindia5750@gmail.com>
2025-04-23 17:09:05 +0530

    Add files via upload

---
+++ models/Content.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const mongoose = require("mongoose");
+const {Schema, model} = require("../connection");
+
+const Content = Schema ({
+    content : {type: String,
+        unique: true,
+        required: [true, "Content is Required"],
+        validate : {
+            validator : function(v)  {
+                return v && v.trim().length > 0;
+            },
+            message: "Content must not be empty",
+        },
+    },
+    timestamp: {type: Date, default: Date.now}
+})
+
+
+
+module.exports = model("content", Content);

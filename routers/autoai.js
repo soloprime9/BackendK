45569aec7665e8808b79d44b9bbfa1633a6b065a
soloprime9 <primeindia5750@gmail.com>
2025-05-14 11:52:53 +0530

    Update autoai.js

--- routers/autoai.js
+++ routers/autoai.js
@@ -6,18 +6,11 @@ const cheerio = require("cheerio");
 
 console.log(process.env.Google_CX_ID, " ", process.env.Google_Search_API)
 
-const ScrappedPushData = [];
 
 
-function CleanScraped(text) {
-    return text
-        .replace(/\s+/g, ' ')           // Collapse multiple spaces/newlines
-        .replace(/(?:\\n|\\t)+/g, ' ')  // Remove escaped newlines/tabs
-        .replace(/<[^>]*>?/gm, '')      // Remove any remaining HTML tags
-        .trim();    
-}
 
-router.get("/search", async(req, res) => {
+
+router.get("/result", async(req, res) => {
     const query = req.query.q;
 
     if(!query){
@@ -56,89 +49,11 @@ router.get("/search", async(req, res) => {
         // console.log(results);
         res.status(200).json(results);
         
-        
-    //     const LinkStore = {};
-    //     results.forEach((item,index) => {
-    //         LinkStore[index] = item.link;
-    //     })
-
-    //     console.log(LinkStore);
-
-    //     const ScrapedData = [];
-
-    //     for (let i = 0; i < Object.keys(LinkStore).length; i++){
-    //         const url = LinkStore[i];
-            
-            
-    //         try {
-    //             const response = await axios.get(url);
-    //             const $ = cheerio.load(response.data);
-
-    //             // Extract title and content 
-
-    //             // const title = $("title").text() || $("h1").text();
-    //             // const content = $("article").text().trim() || $("p").text().trim().slice(0, 100);
-
-    //             const images = [];
-    //             $("img").each((index, element) => {
-    //                 let imageUrl = $(element).attr("src");
-
-    //                 // Handle relative URLs by converting them to absolute URLs
-    //                 if (imageUrl) {
-    //                     // If the image URL is relative, resolve it to an absolute URL
-    //                     if (!imageUrl.startsWith('http') && !imageUrl.startsWith('https')) {
-    //                         imageUrl = url.resolve(urlToScrape, imageUrl);
-    //                     }
-    //                     images.push(imageUrl);
-    //                 }
-    //             });
-                
-    //             ScrapedData.push({
-    //                 // url,
-    //                 // title,
-    //                 // content: content || "No Content",
-    //                 images,
-    //                 results
-
-    //             })
-
-    //             res.status(200).json("Scraping Success: ", ScrapedData);
-                
-
-    //         }
-    //         catch(error){
-    //             console.log(`Scraping Error ${url}:`, error.message);
-    //             ScrapedData.push({
-    //                 url,
-    //                 error:true,
-    //                 message: error.message
-    //             })
-    //         }
-    //     }
-
-    //     // res.json({
-    //     //     total: ScrapedData.length,
-    //     //     data: ScrapedData
-    //     // });
-
-    //     // ScrappedPushData.push(ScrapedData);
-    // }
-
 
 
     catch(error){
         console.log(error);
     }
-
-    
-
-    // const CleanScrappedData = ScrappedPushData.map(item => ({
-    //     ... item,
-    //     content: CleanScraped(item.content)
-        
-    // }));
-
-    // res.json("Cleaned Data : ", CleanScrappedData)
 
 
 

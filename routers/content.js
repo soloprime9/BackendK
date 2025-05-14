23e02e021681c10b0a170dcb06c50d93fbe37b85
soloprime9 <primeindia5750@gmail.com>
2025-05-14 13:33:40 +0530

    Update content.js

--- routers/content.js
+++ routers/content.js
@@ -323,7 +323,7 @@ router.get("/gen", async(req,res) => {
 router.get("/get", async(req, res) => {
     
     try{
-    const detail = await Content.find();
+    const detail = await Content.find().limit(10);
 
     if(detail.length === 0){
         return res.status(400).json("Content not found");

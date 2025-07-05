2497dfee7c08e8f2226b44022b39397d1cf9211a
soloprime9 <primeindia5750@gmail.com>
2025-07-05 17:02:56 +0530

    Update User.js

--- routers/User.js
+++ routers/User.js
@@ -58,7 +58,7 @@ router.post("/login", async (req, res) => {
    }
 
    
-   const token = jwt.sign({UserId: user._id}, process.env.JWT_SECRET, {expiresIn: "1m"} )
+   const token = jwt.sign({UserId: user._id}, process.env.JWT_SECRET, {expiresIn: "30d"} )
    console.log("token:", token);
    return res.status(200).json({UserDetail: user,token: token, message:"Loggin Successfull"});
 

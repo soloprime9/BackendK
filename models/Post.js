f6b3a27a63be320ceb9e21652f196e86f7940aaf
soloprime9 <primeindia5750@gmail.com>
2025-07-07 09:44:47 +0530

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,39 +1,76 @@
-const {Schema, model} = require("../connection");
+const { Schema, model } = require("../connection");
 
+const CommentSchema = new Schema({
+  CommentText: { type: String, required: true },
+  userId: { type: Schema.Types.ObjectId, ref: "User" },
+  likes: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now }
+}, { _id: false });
 
-const PostSchema = Schema ({
-    userId : {type: Schema.Types.ObjectId, ref: "User"},
-    title: {type: String, default : ""},
-    tags : [{type: String, trim: true}],
-    media: {type: String, required: true}, //Cloudary give Url
-    thumbnail: { type: String, default: "" },
-    medias: {
-          type: new Schema({
-            public_id: { type: String, default: "" },
-            url: { type: String, required: true },
-            type: { type: String, required: true }
-          }, { _id: false })  // ✅ _id: false = avoid nested _id
-        },
-
-    mediaType: { type: String, required: true },
-    // likes: [{type: String, default : 0}],
-    likes: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
-
-    comments: [{
-        CommentText: {type: String, required: true},
-        userId : {type: Schema.Types.ObjectId, ref: "User"},
-        likes: {type: Number, default: 0},
-        timestamps: {type: Date, default: Date.now}
-
-    },
-    ],  
-
-},
-
-    {timestamps: true}
-
-
-);
-
+const PostSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: "User" },
+  title: { type: String, default: "" },
+  tags: [{ type: String, trim: true }],
+  media: { type: String, required: true },
+  thumbnail: { type: String, default: "" },
+  medias: {
+    type: new Schema({
+      public_id: { type: String, default: "" },
+      url: { type: String, required: true },
+      type: { type: String, required: true }
+    }, { _id: false })
+  },
+  mediaType: { type: String, required: true },
+  likes: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
+  comments: [CommentSchema],
+}, { timestamps: true });
 
 module.exports = model("Post", PostSchema);
+
+
+
+
+
+
+
+
+
+// const {Schema, model} = require("../connection");
+
+
+// const PostSchema = Schema ({
+//     userId : {type: Schema.Types.ObjectId, ref: "User"},
+//     title: {type: String, default : ""},
+//     tags : [{type: String, trim: true}],
+//     media: {type: String, required: true}, //Cloudary give Url
+//     thumbnail: { type: String, default: "" },
+//     medias: {
+//           type: new Schema({
+//             public_id: { type: String, default: "" },
+//             url: { type: String, required: true },
+//             type: { type: String, required: true }
+//           }, { _id: false })  // ✅ _id: false = avoid nested _id
+//         },
+
+//     mediaType: { type: String, required: true },
+//     // likes: [{type: String, default : 0}],
+//     likes: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
+
+//     comments: [{
+//         CommentText: {type: String, required: true},
+//         userId : {type: Schema.Types.ObjectId, ref: "User"},
+//         likes: {type: Number, default: 0},
+//         timestamps: {type: Date, default: Date.now}
+
+//     },
+//     ],  
+
+// },
+
+//     {timestamps: true}
+
+
+// );
+
+
+// module.exports = model("Post", PostSchema);

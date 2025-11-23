848c26366e7e01bf615bb6eee4439cf7df7738c9
soloprime9 <primeindia5750@gmail.com>
2025-11-23 01:32:33 -0800

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -30,7 +30,7 @@ const PostSchema = new Schema({
     }, { _id: false })
   },
   mediaType: { type: String, required: true },
-  duration: {type: String},
+  duration: { type: Number, default: 0 },
   views : {type: Number, default: 0},
   likes: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
   comments: [CommentSchema],

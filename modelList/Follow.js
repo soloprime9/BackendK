e518515c33022310dee5199fb3879b074189cb5e
soloprime9 <primeindia5750@gmail.com>
2025-12-06 23:43:25 +0530

    Update Follow.js

--- modelList/Follow.js
+++ modelList/Follow.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("../connection");
+const { Schema, model } = require("../connectionList");
 
 const FollowSchema = new Schema({
     follower: { type: Schema.Types.ObjectId, ref: "User" },
@@ -7,3 +7,4 @@ const FollowSchema = new Schema({
 });
 
 module.exports = model("Follow", FollowSchema);
+

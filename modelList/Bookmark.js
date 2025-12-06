af4bd60f8e934ad5721a843796588fc37c0be6ef
soloprime9 <primeindia5750@gmail.com>
2025-12-06 23:41:51 +0530

    Update Bookmark.js

--- modelList/Bookmark.js
+++ modelList/Bookmark.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("../connection");
+const { Schema, model } = require("../connectionList");
 
 const BookmarkSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: "User" },
@@ -7,3 +7,4 @@ const BookmarkSchema = new Schema({
 });
 
 module.exports = model("Bookmark", BookmarkSchema);
+

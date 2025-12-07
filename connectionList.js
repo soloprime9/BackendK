d2831565805670c57b26a885acf5a14cb0065868
soloprime9 <primeindia5750@gmail.com>
2025-12-07 15:36:14 +0530

    Update connectionList.js

--- connectionList.js
+++ connectionList.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
+require("dotenv").config(); // Make sure you have a .env file and dotenv installed
 
+const uri = process.env.MONGODB; // MongoDB URI from your environment variable
 
-mongoose.connect("mongodb+srv://pratikkumar5750:mheekd9tSQqWF3ui@cluster0.x12xsyl.mongodb.net/FondPeace?retryWrites=true&w=majority&appName=Cluster0")
-
-
-
-.then((result) => {
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
     console.log("Connected to Database");
 })
 .catch((error) => {
-    console.log("Not Connected to Database");
-})
+    console.error("Not Connected to Database", error);
+});
 
 module.exports = mongoose;

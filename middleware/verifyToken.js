b0523248a35811eff3fb8207069d68159dd3957c
soloprime9 <primeindia5750@gmail.com>
2025-12-14 14:22:45 +0530

    Update verifyToken.js

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,21 +1,19 @@
 const jwt = require("jsonwebtoken");
 
-const verifyToken = (req, res, next)=> {
-    const token = req.header("x-auth-token");
-    try{
-    if(!token){
-        return res.status(404).json("Bro Token is Required")
-    }
-    
+const verifyToken = (req, res, next) => {
+  const token = req.header("x-auth-token");
+
+  if (!token) {
+    return res.status(401).json({ message: "Token required" });
+  }
+
+  try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    console.log(req.user);
+    req.user = decoded; // { UserId }
     next();
-    }
-    catch(error){
-       return res.status(500).json("Error in Token Checking");
-    }
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
 
-} 
-
-module.exports =verifyToken;
+module.exports = verifyToken;
